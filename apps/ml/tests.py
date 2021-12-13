6c22c36cca69e519a1ea95487841c04eedac42f6
Monxun <michaelgibney1@gmail.com>
2021-12-12 23:39:06 -0700

    Created registry.py / updated tests / successful test

--- apps/ml/tests.py
+++ apps/ml/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
-
+from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
+import inspect
+
 
 class MLTests(TestCase):
     def test_rf_algorithm(self):
@@ -31,3 +33,23 @@ class MLTests(TestCase):
     # constructing an input JSON data object,
     # initializing ML algorithm,
     # computing ML prediction and checking the prediction outcome.
+
+def test_registry(self):
+    registry = MLRegistry()
+    self.assertEqual(len(registry.endpoints), 0)
+    endpoint_name = 'income_classifier'
+    algorithm_object = RandomForestClassifier()
+    algorithm_name = 'random forest'
+    algorithm_status = 'production'
+    algorithm_version = '0.0.1'
+    algorithm_owner = 'Mike'
+    algorithm_description = 'Random Forest with simple pre- and post-processing'
+    algorithm_code = inspect.getsource(RandomForestClassifier)
+    # add to registry
+    registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
+                algorithm_status, algorithm_version, algorithm_owner,
+                algorithm_description, algorithm_code)
+    # there should be one endpoint available
+    self.assertEqual(len(registry.endpoints), 1)
+
+
